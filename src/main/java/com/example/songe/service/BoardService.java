b9e1d97075b77191952483195344d0e6ec211da6
happy5happy5 <zombil8731@gmail.com>
2023-09-03 17:11:59 +0900

    add crud feature

--- src/main/java/com/example/songe/service/BoardService.java
+++ src/main/java/com/example/songe/service/BoardService.java
@@ -11,4 +11,14 @@ public interface BoardService {
     Integer getTotalBoardCount();
 
     List<Board> getBoards(int page, int pageSize);
+
+    Board getBoardById(Long id);
+
+    void updateBoard(Long id, Board updatedBoard);
+
+    boolean checkPassword(Long id, String password);
+
+    void deleteBoard(Long id);
+
+    void createBoard(Board board);
 }
