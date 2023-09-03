c0e7398bdee23a0194efc820342d305d3137b4b9
happy5happy5 <zombil8731@gmail.com>
2023-09-03 17:11:59 +0900

    add crud feature

--- src/main/java/com/example/songe/controller/BoardController.java
+++ src/main/java/com/example/songe/controller/BoardController.java
@@ -7,9 +7,7 @@ import com.example.songe.utils.Statics;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +25,7 @@ public class BoardController {
 
     @GetMapping("/list")
     public String getBoards(Model model, @RequestParam(name = "page", defaultValue = "1") int page) {
+        System.out.println("[Controller GET /list]  getBoards() called");
         int pageSize = Statics.PAGE_SIZE.getValue(); // PAGE_SIZE를 사용하여 페이지당 게시물 수 가져오기
         int blockSize = Statics.BLOCK_SIZE.getValue(); // BLOCK_SIZE를 사용하여 블록당 페이지 수 가져오기
         int totalPosts = boardService.getTotalBoardCount(); // 전체 게시물 수
@@ -47,5 +46,67 @@ public class BoardController {
         return "board/list"; // Thymeleaf 템플릿 파일의 경로를 반환합니다.
     }
 
+    // Controller
+    @GetMapping("/view/{id}")
+    public String viewBoard(@PathVariable Long id, Model model, @RequestParam(name = "error", required = false) String error) {
+        System.out.println("[Controller GET /board/view/{id}]  viewBoard() called");
+        Board board = boardService.getBoardById(id); // boardService에서 해당 게시물 가져오기
 
+        model.addAttribute("board", board);
+        model.addAttribute("error", error);
+
+        return "board/view";
+    }
+
+    // Controller
+    @GetMapping("/edit/{id}")
+    public String editBoard(@PathVariable Long id, Model model, @RequestParam(name = "error", required = false) String error ) {
+        Board board = boardService.getBoardById(id); // boardService에서 해당 게시물 가져오기
+        model.addAttribute("board", board);
+        model.addAttribute("error", error);
+        return "board/edit";
+    }
+
+
+    @PostMapping("/edit/{id}")
+    public String updateBoard(@PathVariable Long id, Board updatedBoard, @RequestParam("password_old") String password) {
+
+        // 비밀번호 확인을 서비스에서 처리
+        if (boardService.checkPassword(id, password)) {
+            // 비밀번호가 일치하면 게시물 삭제 후 목록 페이지로 리다이렉트
+            boardService.updateBoard(id, updatedBoard);
+            return "redirect:/board/view/{id}";
+        } else {
+            // 비밀번호가 일치하지 않으면 오류 메시지와 함께 삭제 페이지로 다시 이동
+            return "redirect:/board/edit/{id}?error=invalidPassword";
+        }
+//        boardService.updateBoard(id, updatedBoard); // boardService에서 게시물 업데이트 처리
+//        return "redirect:/board/view/{id}"; // 수정 후 상세보기 페이지로 이동
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteBoard(@PathVariable Long id, @RequestParam("password") String password) {
+        // 비밀번호 확인을 서비스에서 처리
+        if (boardService.checkPassword(id, password)) {
+            // 비밀번호가 일치하면 게시물 삭제 후 목록 페이지로 리다이렉트
+            boardService.deleteBoard(id);
+            return "redirect:/board/list";
+        } else {
+            // 비밀번호가 일치하지 않으면 오류 메시지와 함께 삭제 페이지로 다시 이동
+            return "redirect:/board/view/{id}?error=invalidPassword";
+        }
+    }
+
+    @GetMapping("/create")
+    public String createBoard() {
+        System.out.println("[Controller GET /board/create]  createBoard() called");
+        return "board/create";
+    }
+
+    @PostMapping("/create")
+    public String createBoard(Board board) {
+        System.out.println("[Controller POST /board/create]  createBoard() called");
+        boardService.createBoard(board);
+        return "redirect:/board/list";
+    }
 }
